7204e673e52053a23546d1248f5970091e290a7d
Alex Kusov <kysov2006@bk.ru>
2022-03-29 07:31:55 +0300

    add elephants on board

--- app.js
+++ app.js
@@ -6,14 +6,16 @@ for( let row = 0; row < 10; row++){
         cell.classList.add('cell');
         cell.dataset.row = row;
         cell.dataset.col = col;
+        cell.style.gridColumn = col + 1;
+        cell.style.gridRow = row + 1;
         gameBoard.append(cell);
     }
 }
 
 const elephants = [
     {
-        x: 0,
-        y: 0,
+        x: 1,
+        y: 1,
         size: 1,
         url: 'https://lh3.googleusercontent.com/kulUKjt-Y12Ld3UeFCwLEGx2GCtdcEEn4GqrikLGyDYXP_x0ERL5UDlAj1paTkiegsd2p1sUPAqANPyRDftT4pEyOF04MF_QNEBZ=w349'
     },
@@ -30,9 +32,22 @@ const elephants = [
         url: 'https://lh3.googleusercontent.com/0U_nKylT2wjfyRRxq0NRJ7YaU-4tkHskFXwztKL2RzfaHnc2x1-wf3y5vSxph7hwKCmBhc2VOthwKbZX22jorg9xBD6T4bfxwE1w2g=w349'
     },
     {
-        x: 0,
-        y: 0,
-        size: 1,
+        x: 2,
+        y: 4,
+        size: 4,
         url: 'https://lh3.googleusercontent.com/1Zc9Cv6vu_6nXUk5lVT6jItvVH_wyMAvtUchd9aSJ4FPiQwAnLEIV2oZe3xss8wGNK5GuWgkeyVrOHBXjUvxEljCVGPhOdaHjFT2TPk=w349'
     }
 ]
+
+
+elephants.forEach((elephant) => {
+    const imgWrapper = document.createElement('div')
+    const img = document.createElement('img');
+    img.src = elephant.url;
+    imgWrapper.append(img);
+    imgWrapper.style.gridRow = elephant.y;
+    imgWrapper.style.gridRowEnd = elephant.y + elephant.size;
+    imgWrapper.style.gridColumn = elephant.x;
+    imgWrapper.style.gridColumnEnd = elephant.x + elephant.size;
+    gameBoard.append(imgWrapper);
+})
