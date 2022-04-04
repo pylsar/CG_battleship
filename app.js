67164f7f310c80e4a85b1d08e90b7ca4c221c22b
Alex Kusov <kysov2006@bk.ru>
2022-04-04 06:50:58 +0300

    bug fixed

--- app.js
+++ app.js
@@ -13,14 +13,14 @@ for( let row = 0; row < 10; row++){
 }
 
 const elephants = [
-    // {
-    //     size: 1,
-    //     url: 'https://lh3.googleusercontent.com/kulUKjt-Y12Ld3UeFCwLEGx2GCtdcEEn4GqrikLGyDYXP_x0ERL5UDlAj1paTkiegsd2p1sUPAqANPyRDftT4pEyOF04MF_QNEBZ=w349'
-    // },
-    // {
-    //     size: 2,
-    //     url: 'https://lh3.googleusercontent.com/wnKMJ_dTIwslgsgTPAdO0fv3jzr6yf3cbpqhcDRf5phQpWrEVbMoopaDg4zvhCeLqcp-yxnKVTUOGpuP5zaCysZ67UjihpKd3_Ohrg=w349'
-    // },
+    {
+        size: 1,
+        url: 'https://lh3.googleusercontent.com/kulUKjt-Y12Ld3UeFCwLEGx2GCtdcEEn4GqrikLGyDYXP_x0ERL5UDlAj1paTkiegsd2p1sUPAqANPyRDftT4pEyOF04MF_QNEBZ=w349'
+    },
+    {
+        size: 2,
+        url: 'https://lh3.googleusercontent.com/wnKMJ_dTIwslgsgTPAdO0fv3jzr6yf3cbpqhcDRf5phQpWrEVbMoopaDg4zvhCeLqcp-yxnKVTUOGpuP5zaCysZ67UjihpKd3_Ohrg=w349'
+    },
     {
         size: 3,
         url: 'https://lh3.googleusercontent.com/0U_nKylT2wjfyRRxq0NRJ7YaU-4tkHskFXwztKL2RzfaHnc2x1-wf3y5vSxph7hwKCmBhc2VOthwKbZX22jorg9xBD6T4bfxwE1w2g=w349'
@@ -60,7 +60,7 @@ elephants.forEach((elephant) => {
     // elephant.x = 1 + Math.floor(Math.random() * 7); // 7 так как size равен 3 а клеток всего 10
     // elephant.y = 1 + Math.floor(Math.random() * 7);
 
-    const others = elephants.filter((other) => other !== elephant);
+    const others = elephants.filter((other) => other !== elephant && other.x && other.y);
     const { x, y } = getRandomeCoordinates(elephant.size, others);
     elephant.x = x;
     elephant.y = y;
